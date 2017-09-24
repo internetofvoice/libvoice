f7832f02eb71a7a4806ba3c61360ecb7f7e7411e
Alexander Schmidt <a.schmidt@internet-of-voice.de>
2017-09-24 08:08:57 +0200

    WIP: Refactor AlexaRequest (Tests)

---
+++ test/Request/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Request;
+
+use \Alexa\Request\User;
+use \PHPUnit\Framework\TestCase;
+
+
+class UserTest extends TestCase {
+	/**
+	 * testUser
+	 */
+	public function testUser() {
+		$fixture = json_decode(file_get_contents(__DIR__ . '/../Fixtures/IntentRequest-Body.txt'), true);
+		$user = new User($fixture['session']['user']);
+		$this->assertStringStartsWith('amzn1.ask.account.', $user->getUserId());
+		$this->assertEquals('lo6539ti9xbd54ng', $user->getAccessToken());
+	}
+}
