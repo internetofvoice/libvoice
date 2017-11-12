64e11add494dbf8a57675f6bb89fa14627bdc7ee
Alexander Schmidt <a.schmidt@internet-of-voice.de>
2017-11-12 18:05:18 +0100

    Extend smarthome tests

--- test/AlexaSmartHome/Request/AlexaRequestTest.php
+++ test/AlexaSmartHome/Request/AlexaRequestTest.php
@@ -13,11 +13,9 @@ use \PHPUnit\Framework\TestCase;
  */
 class AlexaRequestTest extends TestCase {
 	/**
-	 * testAlexaRequest
-	 *
 	 * @group smarthome
 	 */
-	public function testAlexaRequest() {
+	public function testDiscoveryRequest() {
 		$fixture = json_decode(file_get_contents(__DIR__ . '/Fixtures/DiscoveryRequest.json'), true);
 		$alexaRequest = new AlexaRequest($fixture);
 
@@ -38,5 +36,31 @@ class AlexaRequestTest extends TestCase {
         // Test shortcuts
         $this->assertEquals('Alexa.Discovery', $alexaRequest->getHeader()->getNamespace());
         $this->assertEquals('BearerToken', $alexaRequest->getPayload()->getScope()->getType());
+	}
+
+	/**
+	 * @group smarthome
+	 */
+	public function testReportStateRequest() {
+		$fixture = json_decode(file_get_contents(__DIR__ . '/Fixtures/ReportStateRequest.json'), true);
+		$alexaRequest = new AlexaRequest($fixture);
+
+		// Header
+		$this->assertEquals('Alexa', $alexaRequest->getRequest()->getDirective()->getHeader()->getNamespace());
+		$this->assertEquals('ReportState', $alexaRequest->getRequest()->getDirective()->getHeader()->getName());
+		$this->assertEquals('3', $alexaRequest->getRequest()->getDirective()->getHeader()->getPayloadVersion());
+		$this->assertRegExp('/^[0-9a-zA-Z-]{36}$/', $alexaRequest->getRequest()->getDirective()->getHeader()->getMessageId());
+		$this->assertEquals('abcdef-123456', $alexaRequest->getRequest()->getDirective()->getHeader()->getCorrelationToken());
+
+		// Endpoint
+		$this->assertEquals('BearerToken', $alexaRequest->getRequest()->getDirective()->getEndpoint()->getScope()->getType());
+		$this->assertEquals('endpoint-01', $alexaRequest->getRequest()->getDirective()->getEndpoint()->getEndpointId());
+
+		// Context
+		$this->assertEquals('ReConSkillAdapter', $alexaRequest->getContext()->getData()['functionName']);
+
+		// Test shortcuts
+		$this->assertEquals('ReportState', $alexaRequest->getHeader()->getName());
+		$this->assertEquals('BearerToken', $alexaRequest->getEndpoint()->getScope()->getType());
 	}
 }
