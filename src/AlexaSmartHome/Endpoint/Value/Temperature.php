744b9f3b5e77913bf81f2099e6cd56a65fe9c557
Alexander Schmidt <a.schmidt@internet-of-voice.de>
2017-11-16 12:49:30 +0100

    Smart home: Add (object) Values

---
+++ src/AlexaSmartHome/Endpoint/Value/Temperature.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace InternetOfVoice\LibVoice\AlexaSmartHome\Endpoint\Value;
+
+use \InvalidArgumentException;
+
+/**
+ * Class Temperature
+ *
+ * @author  Alexander Schmidt <a.schmidt@internet-of-voice.de>
+ * @license http://opensource.org/licenses/MIT
+ */
+class Temperature {
+    /** @var float $value */
+    protected $value;
+
+    /** @var array validScales */
+    const validScales = ['CELSIUS', 'FAHRENHEIT', 'KELVIN'];
+
+    /** @var string $scale */
+    protected $scale;
+
+
+    /**
+     * @param float  $value
+     * @param string $scale
+     */
+    public function __construct($value = null, $scale = null) {
+        $this->setValue($value);
+        $this->setScale($scale);
+    }
+
+    /**
+     * @return Temperature()
+     */
+    public static function create() {
+        return new Temperature();
+    }
+
+
+    /**
+     * @return float
+     */
+    public function getValue() {
+        return $this->value;
+    }
+
+    /**
+     * @param  float $value
+     * @return Temperature
+     */
+    public function setValue($value) {
+        $this->value = floatval($value);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScale() {
+        return $this->scale;
+    }
+
+    /**
+     * @param  string $scale
+     * @return Temperature
+     * @throws InvalidArgumentException
+     */
+    public function setScale($scale) {
+        if(!in_array($scale, self::validScales)) {
+            throw new InvalidArgumentException('Scale must be one of: ' . implode(', ', self::validScales));
+        }
+
+        $this->scale = $scale;
+        return $this;
+    }
+
+
+	/**
+	 * @return array
+     * @throws InvalidArgumentException
+	 */
+	public function render() {
+	    $value = $this->getValue();
+	    $scale = $this->getScale();
+
+        if(is_null($value) or is_null($scale)) {
+            throw new InvalidArgumentException('Value and Scale are required.');
+        }
+
+        $rendered = [
+            'value' => $value,
+            'scale' => $scale,
+        ];
+
+        return $rendered;
+	}
+}
