7f9fd71e2c68b0cb3d6e9680d44ebdd29d62146d
Alexander Schmidt <a.schmidt@internet-of-voice.de>
2017-11-30 20:03:58 +0100

    Add apiAccessToken to System object

--- test/Alexa/Request/Context/SystemTest.php
+++ test/Alexa/Request/Context/SystemTest.php
@@ -17,10 +17,12 @@ class SystemTest extends TestCase {
 	 */
 	public function testSystem() {
 		$fixture = json_decode(file_get_contents(__DIR__ . '/../Fixtures/IntentRequest-Body.txt'), true);
+		$fixture['context']['System']['apiAccessToken'] = 'apiAccessToken';
 		$system = new System($fixture['context']['System']);
         $this->assertEquals('InternetOfVoice\LibVoice\Alexa\Request\Application', get_class($system->getApplication()));
         $this->assertEquals('InternetOfVoice\LibVoice\Alexa\Request\User', get_class($system->getUser()));
         $this->assertEquals('InternetOfVoice\LibVoice\Alexa\Request\Context\System\Device', get_class($system->getDevice()));
         $this->assertEquals('https://api.eu.amazonalexa.com', $system->getApiEndpoint());
+        $this->assertEquals('apiAccessToken', $system->getApiAccessToken());
 	}
 }
