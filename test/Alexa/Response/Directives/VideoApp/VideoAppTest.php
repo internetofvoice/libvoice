b048716be5fe25ee3493e74cf55093a010f83fef
Alexander Schmidt <a.schmidt@internet-of-voice.de>
2017-12-02 21:16:58 +0100

    Add VideoApp

---
+++ test/Alexa/Response/Directives/VideoApp/VideoAppTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Alexa\Response\Directives\VideoApp;
+
+use \InternetOfVoice\LibVoice\Alexa\Response\Directives\VideoApp\Launch;
+use \InternetOfVoice\LibVoice\Alexa\Response\Directives\VideoApp\VideoItem;
+use \PHPUnit\Framework\TestCase;
+
+/**
+ * Class VideoAppTest
+ *
+ * @author  Alexander Schmidt <a.schmidt@internet-of-voice.de>
+ * @license http://opensource.org/licenses/MIT
+ */
+class VideoAppTest extends TestCase {
+	public function testVideoItem() {
+		$videoItem = new VideoItem('https://example.com', 'Title', 'Subtitle');
+		$expect = [
+			'source' => 'https://example.com',
+			'metadata' => [
+				'title' => 'Title',
+				'subtitle' => 'Subtitle',
+			]
+		];
+
+		$this->assertEquals($expect, $videoItem->render());
+	}
+
+	public function testLaunch() {
+		$launch = new Launch(new VideoItem('https://example.com', 'Title', 'Subtitle'));
+		$expect = [
+			'type' => 'VideoApp.Launch',
+			'videoItem' => [
+				'source' => 'https://example.com',
+				'metadata' => [
+					'title' => 'Title',
+					'subtitle' => 'Subtitle',
+				]
+			]
+		];
+
+		$this->assertEquals($expect, $launch->render());
+	}
+}
